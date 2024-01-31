873fac179fe1ab2840c9e2c033ebd252a99a329b
EC2 Default User <ec2-user@ip-172-31-21-83.ap-southeast-2.compute.internal>
2024-01-31 21:59:12 +0000

    [Update]ジャンル追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,3 +1,20 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
 end
+
+def genres
+    
+end
+
+def create
+  @genre=Genre.new(genre_params)
+  @genre.save
+  redirect_to admin_genres_path
+end
+
+
+private
+
+def genre_params
+  params.require(:genre).permit(:genre_name)
+end
