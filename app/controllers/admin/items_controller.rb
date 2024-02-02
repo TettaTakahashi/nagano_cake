121d603ca56cc317d88ca93d95cd728f36ac57e0
EC2 Default User <ec2-user@ip-172-31-21-83.ap-southeast-2.compute.internal>
2024-02-02 10:51:19 +0000

    [Update]商品新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,3 +1,23 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
+  
+  def new
+      @item=Item.new
+  end
+  
+  def create
+      @item=Item.new(item_params)
+      @item.save
+      redirect_to admin_item_path
+  end
+  
+  def show
+      
+  end
+  
+  private
+  
+  def item_params
+      params.require(:item).permit(:image, :name, :introduction)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
     get 'homes/top'
     resources :genres, only: [:index, :create]
     get 'genres' => 'genres#index'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
