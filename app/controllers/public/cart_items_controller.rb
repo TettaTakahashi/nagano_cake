68fd4035aee4c178935c73f2e38a15be7f5f0898
EC2 Default User <ec2-user@ip-172-31-21-83.ap-southeast-2.compute.internal>
2024-02-10 15:01:59 +0000

    [Update]カート内商品一覧画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,8 +9,15 @@ class Public::CartItemsController < ApplicationController
       @cart_item=CartItem.new(cart_item_params)
       @cart_item.customer_id=current_customer.id
       @cart_item.item_id=cart_item_params[:item_id]
-      @cart_item.save
-      redirect_to cart_items_path
+      if @cart_item.find_by(item_id: params[:cart_item][:item_id]).present?
+        @cart_item=CartItem.find_by(item_id: params[:cart_item][:item_id])
+        @cart_item.amount+=params[:cart_item][:amount].to_i
+        @cart_item.update(amount: cart_item.amount)
+        redirect_to cart_items_path
+      else
+        @cart_item.save
+        redirect_to cart_items_path
+      end
   end
   
   def update
@@ -23,6 +30,11 @@ class Public::CartItemsController < ApplicationController
     @cart_item=CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path
+  end
+  
+  def destroy_all
+    CartItem.destroy_all
+    redirect_back(fallback_location: root_path)
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
     get 'cart_items' => 'cart_items#index'
     patch 'cart_items/:id' => 'cart_items#update'
     delete 'cart_items/:id' => 'cart_items#destroy'
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    get 'orders/new' => 'orders#new'
     
   end
   
